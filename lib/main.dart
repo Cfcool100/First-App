2f6ddf26fb74bfa815a8fd62fe675ec38963b25e
Cfcool100 ~ % git config --global user.name Youssouf <cfcool100@gmail.com>
2023-08-20 22:57:32 +0000

    v.0.1

---
+++ lib/main.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          toolbarHeight: 45,
+          title: const Row(
+            children: [
+              Text('Asynconf 2022'),
+            ],
+          ),
+        ),
+        body: const Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                'Asynconf 2022',
+                style: TextStyle(
+                  fontSize: 42,
+                  fontWeight:FontWeight.bold,
+                ),
+              ),
+                // ajouter un padding
+              Text(
+                'Salon virtuel de informatique. Du 27 au 29 deoctobre 2k22.',
+                style: TextStyle(
+                  fontSize: 24,
+                ),
+                textAlign: TextAlign.center,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
