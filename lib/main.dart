de2fdd7a835209a74762e15354732d1726668903
Cfcool100 ~ % git config --global user.name Youssouf <cfcool100@gmail.com>
2023-08-21 23:59:08 +0000

    adding button

--- lib/main.dart
+++ lib/main.dart
@@ -10,43 +10,86 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        appBar: AppBar(
-          toolbarHeight: 45,
-          title: const Row(
-            children: [
-              Text('Asynconf 2022'),
-            ],
-          ),
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        toolbarHeight: 45,
+        title: const Row(
+          children: [
+            Text('Asynconf 2022'),
+          ],
         ),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              SvgPicture.asset(
-                'assets/images/logo.svg',
-                color: Colors.blue,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            SvgPicture.asset(
+              'assets/images/logo.svg',
+              color: Colors.blue,
+            ),
+            const Text(
+              'Asynconf 2022',
+              style: TextStyle(
+                fontSize: 42,
+                fontFamily: 'Poppins',
+                fontWeight:FontWeight.bold,
               ),
-              const Text(
-                'Asynconf 2022',
-                style: TextStyle(
-                  fontSize: 42,
-                  fontFamily: 'Poppins',
-                  fontWeight:FontWeight.bold,
-                ),
+            ),
+            const Text(
+              'Salon virtuel de informatique. Du 27 au 29 deoctobre 2k22.',
+              style: TextStyle(
+                fontSize: 24,
               ),
-              const Text(
-                'Salon virtuel de informatique. Du 27 au 29 deoctobre 2k22.',
-                style: TextStyle(
-                  fontSize: 24,
-                ),
-                textAlign: TextAlign.center,
-              ),
-            ],
-          ),
+              textAlign: TextAlign.center,
+            ),
+            ElevatedButton(onPressed: () => print('yo'),
+             child: Text('I had a planning')
+             )
+          ],
         ),
+      ),
+    );
+  }
+}
+
+
+// Planning page
+
+class PlanningPage extends StatelessWidget {
+  const PlanningPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Row(
+          children: [
+            Text('Plannings du Salon'),
+          ],
+        ),
+      ),
+      body: const Center(
+        child: 
+        Text(
+          'Soon available !',
+          style: TextStyle(
+                fontSize: 24,
+              )
+        )
       ),
     );
   }
